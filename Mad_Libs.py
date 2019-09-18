913a187c792d79fb80b1fd3a7d3a47963b9106d2
Nasanut11 <54650016+Nasanut11@users.noreply.github.com>
2019-09-18 19:02:21 -0400

    Update Mad_Libs.py

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -1,8 +1,12 @@
-import random
 #%%
-verbs = ["run","walk","fly","climb","swim"]
+import random
+verbs = ["runs","walks","milks","flys","climbs","swims"]
 nouns = ["man","woman","ocean","cat","dog"]
-names = ["Disney","Australia","Orlando","Wawa","Casablanca"]
-print(verbs)
-print(nouns)
-print(names)
+names = ["Disney","Australia","Orlando","Wawa","Casablanca","Starbucks"]
+adjectives = ["gargantuan","gaseous","hairy","damaged","baggy","bare","eager"]
+
+
+print(random.choice(verbs))
+print(random.choice(nouns))
+print(random.choice(names))
+print(random.choice(adjectives))
