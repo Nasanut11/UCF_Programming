4660008d5b73b6520b523412aa56608a1fd4a8fd
Nasanut11 <54650016+Nasanut11@users.noreply.github.com>
2019-09-18 18:40:25 -0400

    now

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -1,4 +1,8 @@
 import random
 #%%
-verbs = ["hello","b","c"]
+verbs = ["run","walk","fly","climb","swim"]
+nouns = ["man","woman","ocean","cat","dog"]
+pronouns = ["Disney","Australia","Orlando","Wawa","Casablanca"]
 print(verbs)
+print(nouns)
+print(pronouns)
