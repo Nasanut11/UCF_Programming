cbd17ca2ed4d8a1e9a62241b39bc3c5d0ec4e3f0
Nasanut11 <54650016+Nasanut11@users.noreply.github.com>
2019-09-25 20:19:15 -0400

    many mistakes

--- plus1.py
+++ plus1.py
@@ -1,5 +1,6 @@
 #%%
 
+#This is an example ofhow to make a function
 def addone(y):
     return y+1
 
@@ -10,3 +11,8 @@ addone(x)
 #%%
 x= addone(x)
 print(x)
+
+#%%
+
+for x in [2,3,4]:
+    print(x**x)
