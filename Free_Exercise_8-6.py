bc5afdc511a0b28dfe600e8dd034c810ab6ab2d4
Nasanut11 <jfherman13@yahoo.com>
2019-10-09 10:24:32 -0400

    new new

---
+++ Free_Exercise_8-6.py
@@ -0,0 +1,11 @@
+#%%
+def devowel(word):
+    List = list(word)
+    Vowels = ["a", "e", "i", "o", "u"]
+    for letter in List:
+        if letter.lower() in Vowels:
+            List.remove(letter)
+    word = ''.join(List)
+    return word
+
+#%%
