3a8e7b22d32efe0b8d59867770feb1a753c750c7
Nasanut11 <54650016+Nasanut11@users.noreply.github.com>
2019-10-23 20:12:22 -0400

    h

--- InClass_Ex/re.py
+++ InClass_Ex/re.py
@@ -18,3 +18,12 @@ zipcode2 = "12345-6789"
 x = re.search("^\d{5}$", zipcode1)
 y = re.search("^\d{5}(-\d{4})?$", zipcode2)
 print(x, y)
+
+#%%
+import re
+time1 = "5pm"
+time2 = "5am"
+time3 = "17:00"
+x = re.search("^([1-9]|1[0-2])(:\d\d)?[ap]m?$", time1)
+print(x)
+ 
